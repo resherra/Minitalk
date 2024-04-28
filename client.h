b4a38ee26c05663cf29b6540b8b6aa994800c4e8
Redouan Cherrat <recherra@c2r5p2.1337.ma>
2024-04-28 17:45:59 +0100

    preprod

---
+++ client.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 16:33:32 by recherra          #+#    #+#             */
+/*   Updated: 2024/04/28 16:33:33 by recherra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INIT_H
+#define INIT_H
+
+#include "libft/libft.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+//write, signal, sigemptyset, sigaddset, sigaction, kill, getpid, malloc, free, pause, sleep, usleep, exit
+
+
+
+#endif
---
+++ server.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 16:33:32 by recherra          #+#    #+#             */
+/*   Updated: 2024/04/28 16:33:33 by recherra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef INIT_H
+#define INIT_H
+
+#include "libft/libft.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+
+//write, ft_printf and any equivalent YOU coded, signal, sigemptyset, sigaddset, sigaction, kill, getpid, malloc, free, pause, sleep, usleep, exit
+
+
+
+#endif
